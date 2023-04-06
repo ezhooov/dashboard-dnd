b2d5ec12f07e3ff0e1a1d92de13931c6b25da328
Ilia Ezhov <ezhoooov@gmail.com>
2023-04-07 01:47:57 +0400

    downgrade typescript to 4.9 (my version of WS doesn't work with 5.0), update tsconfig, sync TS checks with dev version (add and setup vite-plugin-checker, add tsc check at precommit hook), slightly update eslintrc, add react-router-dom and follow few steps of new version instruction

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root, { rootAction, rootLoader } from './routes/Root'
+import ErrorPage from './ErrorPage'
+import Contact from './routes/Contact'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
